3c64316c5d46dcc87267384365e27dae291d777d
erennbaspinar <ishakeren2001@gmail.com>
2022-04-25 02:33:25 +0300

    Quick sort added.

---
+++ src/QuickSort.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class QuickSort {
+
+    private static ArrayList<Integer> readInput(String fileName) {
+        try {
+            File myObj = new File(fileName);
+            Scanner input = new Scanner(myObj);
+            ArrayList<Integer> array = new ArrayList<>();
+            while (input.hasNextLine()) {
+                String data = input.nextLine();
+                array.add(Integer.parseInt(data));
+            }
+            input.close();
+            return array;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int partition(ArrayList<Integer> array, int start, int end) {
+        int pivot = array.get(start);
+
+        while(start<=end) {
+            while(array.get(start) < pivot)
+                start++;
+
+            while(array.get(end) > pivot)
+                end--;
+
+            if(start<=end) {
+                int tmp = array.get(start);
+                array.set(start, array.get(end));
+                array.set(end, tmp);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+    public static void quickSort(ArrayList<Integer> array, int start, int end) {
+        int index = partition(array, start, end);
+
+        if(start < index-1) {
+            quickSort(array, start, index-1);
+        }
+        if(end > index) {
+            quickSort(array, index, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> array = readInput("Inputs/randomDistribution.txt");
+        System.out.print("Enter element index k: ");
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        try {
+            if (array != null) {
+                quickSort(array, 0, array.size() - 1);
+                System.out.println(array.get(k - 1));
+            } else {
+                System.out.println("Input file empty.");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds.");
+        }
+    }
+}
