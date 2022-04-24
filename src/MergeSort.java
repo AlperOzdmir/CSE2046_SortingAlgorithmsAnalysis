7da4813c3ac10fb3a92ae6226d4b9d69367b2769
erennbaspinar <ishakeren2001@gmail.com>
2022-04-25 01:02:11 +0300

    Merge sort added

---
+++ src/MergeSort.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MergeSort {
+    private static ArrayList<Integer> readInput(String fileName) {
+        try {
+            File myObj = new File(fileName);
+            Scanner input = new Scanner(myObj);
+            ArrayList<Integer> array = new ArrayList<>();
+            while (input.hasNextLine()) {
+                String data = input.nextLine();
+                array.add(Integer.parseInt(data));
+            }
+            input.close();
+            return array;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void divideArray(ArrayList<Integer> array, int indexStart, int indexEnd) {
+
+        if (indexStart < indexEnd && (indexEnd - indexStart) >= 1) {
+            int middleElement = (indexEnd + indexStart) / 2;
+
+            divideArray(array, indexStart, middleElement);
+            divideArray(array,middleElement + 1, indexEnd);
+
+            mergeArray(array, indexStart, middleElement, indexEnd);
+        }
+    }
+
+    public static void mergeArray(ArrayList<Integer> array, int indexStart, int indexMiddle, int indexEnd) {
+
+        ArrayList<Integer> tempArray = new ArrayList<>();
+
+        int tempLeftIndex = indexStart;
+        int tempRightIndex = indexMiddle + 1;
+
+        while (tempLeftIndex <= indexMiddle && tempRightIndex <= indexEnd) {
+
+            if (array.get(tempLeftIndex) <= array.get(tempRightIndex)) {
+                tempArray.add(array.get(tempLeftIndex));
+                tempLeftIndex++;
+            } else {
+                tempArray.add(array.get(tempRightIndex));
+                tempRightIndex++;
+            }
+        }
+
+        while (tempLeftIndex <= indexMiddle) {
+            tempArray.add(array.get(tempLeftIndex));
+            tempLeftIndex++;
+        }
+
+        while (tempRightIndex <= indexEnd) {
+            tempArray.add(array.get(tempRightIndex));
+            tempRightIndex++;
+        }
+
+        for (int i = 0; i < tempArray.size(); indexStart++)
+            array.set(indexStart, tempArray.get(i++));
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> array = readInput("Inputs/randomDistribution.txt");
+        System.out.print("Enter element index k: ");
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        try {
+            if (array != null) {
+                divideArray(array, 0, array.size() - 1);
+                System.out.println(array.get(k - 1));
+            } else {
+                System.out.println("Input file empty.");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bounds.");
+        }
+    }
+}
+
+
