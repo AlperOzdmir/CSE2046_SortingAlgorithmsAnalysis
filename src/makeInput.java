25cb3f5d3a0f36700818bc9c5d91a653a0dbe255
Tegralyn <tegralyn@gmail.com>
2022-05-02 16:07:49 +0300

    size1k.txt updated.

--- src/makeInput.java
+++ src/makeInput.java
@@ -6,15 +6,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class makeInput {
     public static void main(String[] args) {
         try {
-            FileWriter myWriter = new FileWriter("Inputs/randomDistribution.txt");
+            FileWriter myWriter = new FileWriter("Inputs/size10k.txt");
             int min = 1;
-            int max = 5000;
+            int max = 10000;
             int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
             ArrayList<Integer> list = new ArrayList<>();
             list.add(randomNum);
             myWriter.write(randomNum + "\n");
             boolean isDuplicate = false;
-            while (list.size() < 5000) {
+            while (list.size() < 10000) {
                 randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
                 for (Integer integer : list) {
                     if (integer == randomNum) {
