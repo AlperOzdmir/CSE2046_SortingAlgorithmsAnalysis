8f4f1e1406854709ec9350ebf4878cc438b4d7d9
emrhnerdogan01 <emrhnerdogan01@gmail.com>
2022-05-10 21:44:16 +0300

    QuickSelectFirst and QuickSelectMedian are done.

--- src/QuickSelectFirst.java
+++ src/QuickSelectFirst.java
@@ -24,25 +24,64 @@ public class QuickSelectFirst {
     }
 
     // Write this method. Choose the first element as the pivot.
-    /*
+    public static int partition(ArrayList<Integer> array, int low,
+                                int high)
+    {
+        int pivot = array.get(low), pivotloc = high;
+        for (int i = high; i >= low; i--) {
+            // inserting elements of less value
+            // to the left of the pivot location
+            if (array.get(i) > pivot) {
+                int temp = array.get(i);
+                array.set(i, array.get(pivotloc));
+                array.set(pivotloc, temp);
+                pivotloc--;
+            }
+        }
 
+        // swapping pivot to the final pivot location
+        int temp = array.get(low);
+        array.set(low, array.get(pivotloc));
+        array.set(pivotloc, temp);
 
-    public static void quickSelect(ArrayList<Integer> array){
-
+        return pivotloc;
     }
 
 
-    */
+
+    static int quickSelect(ArrayList<Integer> array, int low, int high, int k){
+         int partPos = partition(array, low, high);
+        // if partition value is equal to the kth position,
+        // return value at k.
+        if (partPos == k - 1)
+            return array.get(partPos);
+
+        // if partition value is less than kth position,
+        // search right side of the array.
+        else if (partPos < k - 1)
+            return quickSelect(array, partPos + 1, high, k);
+
+        // if partition value is more than kth position,
+        // search left side of the array.
+        else
+            return quickSelect(array, low, partPos - 1, k);
+    }
+
+
+
 
 
     public static void main(String[] args) {
         ArrayList<Integer> array = readInput("Inputs/randomDistribution.txt");
+        ArrayList<Integer> arrayCopy = array;
         System.out.print("Enter element index k: ");
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
+        int length = array.size();
         try {
             if (array != null) {
                 // Call the sort method here
+                quickSelect(arrayCopy, 0, length - 1, k);
                 System.out.println(array.get(k-1));
             } else {
                 System.out.println("Input file empty.");
@@ -50,5 +89,6 @@ public class QuickSelectFirst {
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Index out of bounds.");
         }
+
     }
 }
--- src/QuickSelectMedian.java
+++ src/QuickSelectMedian.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class QuickSelectMedian {
 
@@ -22,19 +23,96 @@ public class QuickSelectMedian {
             return null;
         }
     }
+    public static int medianPivot(ArrayList<Integer> array, int first, int last) { //Find the median pivot for Quick Sort with median of three
 
-    // Write this method. This time use median-of-three pivot selection.
+        //Sort array for first, last and middle elements and use the second largest element as pivot
 
-    /*
+        int mid = (last) / 2;
 
+        int[] sortingArr = {array.get(first), array.get(mid), array.get(last)};
+        Arrays.sort(sortingArr);
 
-    public static void quickSelect(ArrayList<Integer> array){
+        //System.out.println("\tMiddle of Arr at Index= " + mid + " : " + array[mid]);
+        int middleValue = sortingArr[1];
 
+        //System.out.println("\t"+Arrays.toString(sortingArr));
+        //Swap with the last for pivot
+        int temp = array.get(last);
+        array.set(last, middleValue);
+        if (middleValue == array.get(first)) {
+            array.set(first, temp);
+        } else if (middleValue == array.get(mid)) {
+            array.set(mid, temp);
+        }
+        return partition(array, first, last);
+    }
+    public static void medianQuickSelect(ArrayList<Integer> array, int low, int high, int k) { //Method for Quick Sort with median-of-three pivot selection
+        if (low >= high)
+            return;
+
+        if (low < high) {
+
+            int pivot = medianPivot(array, low, high);
+            quickSelect(array, low, high, k);
+        }
+    }
+    static int quickSelect(ArrayList<Integer> array, int low, int high, int k){
+        if (k > 0 && k <= high - low + 1)
+        {
+
+            int pos = partition(array, low, high);
+
+            if (pos-low == k-1)
+                return array.get(pos);
+
+            if (pos-low > k-1)
+                return quickSelect(array, low, pos-1, k);
+
+            return quickSelect(array, pos+1, high, k-pos+low-1);
+        }
+
+        // If k is more than number of elements in array
+        return Integer.MAX_VALUE;
     }
 
+    static void swap(ArrayList<Integer> array, int i, int j) {
+        int temp = array.get(i);
+        array.set(i, array.get(j));
+        array.set(j, temp);
 
-    */
+    }
+    public static int partition(ArrayList<Integer> array, int low,
+                                int high)
+    {
 
+        int pivot = array.get(high), pivotloc = low;
+        for (int i = low; i <= high; i++) {
+            // inserting elements of less value
+            // to the left of the pivot location
+            if (array.get(i) < pivot) {
+                swap(array, pivotloc, i);
+                pivotloc++;
+            }
+        }
+        swap(array,pivotloc, high);
+
+        // swapping pivot to the final pivot location
+
+        return pivotloc;
+    }
+    static int medianThree(ArrayList<Integer> array, int l, int h) {
+        int pivot;
+        if (((array.get(l) > array.get(h)) && (array.get(l) < array.get(h/2))) || ((array.get(l) < array.get(h)) &&
+                array.get(l) > array.get(h/2)))
+            pivot = array.get(l);
+        else if (((array.get(h) > array.get(l)) && (array.get(h) < array.get(h/2))) || ((array.get(h) < array.get(l)) &&
+                array.get(h) > array.get(h/2)))
+            pivot = array.get(h);
+        else
+            pivot = array.get(h/2);
+        swap(array, pivot + l, h);
+        return partition(array, l, h);
+    }
 
     public static void main(String[] args) {
         ArrayList<Integer> array = readInput("Inputs/randomDistribution.txt");
@@ -44,6 +122,7 @@ public class QuickSelectMedian {
         try {
             if (array != null) {
                 // Call the sort method here
+                medianQuickSelect(array, 0, array.size() - 1, k);
                 System.out.println(array.get(k-1));
             } else {
                 System.out.println("Input file empty.");
@@ -51,5 +130,6 @@ public class QuickSelectMedian {
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Index out of bounds.");
         }
+
     }
 }
