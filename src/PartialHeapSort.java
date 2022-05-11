cf2234a18f11ff706b74c2d22ce7d27ef0ddf1ca
erennbaspinar <ishakeren2001@gmail.com>
2022-05-11 14:33:45 +0300

    Heap Sort

--- src/PartialHeapSort.java
+++ src/PartialHeapSort.java
@@ -23,55 +23,65 @@ public class PartialHeapSort {
         }
     }
 
-    static void heapify(ArrayList<Integer> array, int n, int i)
-    {
-        int largest = i; // Initialize largest as root
-        int left = 2 * i + 1; // left child
-        int right = 2 * i + 2; // right child
-        // If left child is larger than root
-        if (left < n && array.get(left) > array.get(largest))
-            largest = left;
-        // If right child is larger than root
-        if (right < n && array.get(right) > array.get(largest))
-            largest = right;
-        // If root is not largest
-        if (largest != i) {
-            // swap a[i] with a[largest]
-            int temp = array.get(i);
-            array.set(i, array.get(largest));
-            array.set(largest, temp);
 
-            heapify(array, n, largest);
-        }
-    }
-
-    static void heapSort(ArrayList<Integer> array, int n)
+    public static void sort(ArrayList<Integer> array)
     {
+        int n = array.size();
+
+        // Build heap (rearrange array)
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(array, n, i);
 
         // One by one extract an element from heap
-        for (int i = n - 1; i >= 0; i--) {
-            /* Move current root element to end*/
-            // swap a[0] with a[i]
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
             int temp = array.get(0);
             array.set(0, array.get(i));
-
             array.set(i, temp);
 
+            // call max heapify on the reduced heap
             heapify(array, i, 0);
         }
     }
 
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    public static void heapify(ArrayList<Integer> array, int n, int i)
+    {
+        int largest = i;  // Initialize largest as root
+        int l = 2*i + 1;  // left = 2*i + 1
+        int r = 2*i + 2;  // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && array.get(l) > array.get(largest))
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && array.get(r) > array.get(largest))
+            largest = r;
+
+        // If largest is not root
+        if (largest != i)
+        {
+            int swap = array.get(i);
+            array.set(i, array.get(largest));
+            array.set(largest, swap);
+            // Recursively heapify the affected sub-tree
+            heapify(array, n, largest);
+        }
+    }
+
+
     public static void main(String[] args) {
-        ArrayList<Integer> array = readInput("Inputs/randomDistribution.txt");
+        ArrayList<Integer> array = readInput("Inputs/size10k.txt");
         System.out.print("Enter element index k: ");
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
+
         try {
             if (array != null) {
-                //sort(array, k);
-                heapSort(array, k);
+                sort(array);
                 System.out.println(array.get(k-1));
             } else {
                 System.out.println("Input file empty.");
